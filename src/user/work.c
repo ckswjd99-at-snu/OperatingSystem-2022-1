ca33ded8c7836c6f6792d2516c696c2189d5e53a
ckswjd99 <ckswjd99@snu.ac.kr>
2022-05-24 11:43:02 +0900

    fix work.c

--- src/user/work.c
+++ src/user/work.c
@@ -10,15 +10,15 @@ eos_tcb_t tcb2; // tcb for task2
 eos_tcb_t tcb3; // tcb for task3
 
 void task1() {
-	while(1) { PRINT(“A\n”); eos_sleep(0); }// ‘A’ 출력 후 다음 주기까지 기다림
+	while(1) { PRINT ("A\n"); eos_sleep(0); } // ‘A’ 출력 후 다음 주기까지 기다림
 }
 
 void task2() {
-	while(1) { PRINT(“B\n”); eos_sleep(0); }// ‘B’ 출력 후 다음 주기까지 기다림
+	while(1) { PRINT("B\n"); eos_sleep(0); }// ‘B’ 출력 후 다음 주기까지 기다림
 }
 
 void task3() {
-	while(1) { PRINT(“C\n”); eos_sleep(0); }// ‘C’ 출력 후 다음 주기까지 기다림
+	while(1) { PRINT("C\n"); eos_sleep(0); }// ‘C’ 출력 후 다음 주기까지 기다림
 }
 
 void eos_user_main() {
@@ -27,7 +27,7 @@ void eos_user_main() {
 
 	eos_create_task(&tcb2, stack2, STACK_SIZE, task2, NULL, 10); // 태스크 2 생성
 	eos_set_period(&tcb2, 4);
-	
+
 	eos_create_task(&tcb3, stack3, STACK_SIZE, task3, NULL, 50); // 태스크 3 생성
 	eos_set_period(&tcb3, 8);
 }
